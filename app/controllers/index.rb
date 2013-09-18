db3e75c7516e81f2d9d2a8c2ff127dab58d73314
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 14:52:28 -0500

    Final

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,22 +1,35 @@
 get '/' do
   # Look in app/views/index.erb
+
+  # Grab all of the URLs, saving them to an instance variable
+  # Iterate over them to display their values in the view
+  @urls = Url.all
+  
   erb :index
 end 
 
 get '/:short_url' do
   @url = Url.where("short_url = '#{params[:short_url]}'").first
+  @url.count_up
   redirect to (@url.long_url)
+
 end
 
 
 #=====POST===================
 
 post '/urls' do
+  # Save the new url
+  # If successful, redirect back to home page
+  # Else, handle error
 
   url = Url.random_generator
   short_url = Url.new(long_url: params[:long_url], short_url: url)
+
   if short_url.save
-    redirect to('/')
+    @url_1 = "http://localhost:9393/" + url
+    
+    redirect to ("/")
   else
     erb :index
   end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,12 +1,17 @@
 class Url < ActiveRecord::Base
   # Remember to create a migration!
-  validates :long_url, length: {minimum: 1}
-  validates :long_url, format: { with: /https*:\/\/\S+/, message: "Must be a website"}
+  #validates :long_url, length: {minimum: 1}
+  #validates :long_url, format: { with: /https*:\/\/\S+/, message: "Must be a website"}
 
   
 
   def self.random_generator
      ('a'..'z').to_a.sample(5).join
+  end
+
+  def count_up
+    self.click_counter += 1
+    self.save
   end
 
 end
