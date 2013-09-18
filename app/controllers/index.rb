7ef724babd304e1797441a53706bdaea21234caf
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 11:00:37 -0500

    routes and model complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,23 @@
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end 
+
+get '/:short_url' do
+  @url = Url.where("short_url = '#{params[:short_url]}'").first
+  redirect to (@url.long_url)
+end
+
+
+#=====POST===================
+
+post '/urls' do
+
+  url = Url.random_generator
+  short_url = Url.new(long_url: params[:long_url], short_url: url)
+  if short_url.save
+    redirect to('/')
+  else
+    erb :index
+  end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,12 @@
 class Url < ActiveRecord::Base
   # Remember to create a migration!
+  validates :long_url, length: {minimum: 1}
+  validates :long_url, format: { with: /https*:\/\/\S+/, message: "Must be a website"}
+
+  
+
+  def self.random_generator
+     ('a'..'z').to_a.sample(5).join
+  end
+
 end
